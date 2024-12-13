532d9f63c9f6864cf010e5b54263381ed3345a12
Osama <osama.qalam@hotmail.com>
2024-12-12 22:46:16 -0500

    Added a start button

---
+++ Assets/Scripts/StartButtonHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class StartButtonHandler : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Debug.Log("Button Pressed!");
+        // Add your custom functionality here
+    }
+}
